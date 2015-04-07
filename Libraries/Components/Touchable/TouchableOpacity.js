aa69499a096027d5d1f72b5358f171089bcc0de4
James Ide <ide@jameside.com>
2015-04-06 18:30:26 -0700

    [Touchable] Expose the touch-retention offset as a prop
    The touch-retention offset defines a rect around a touchable component in which the touch is retained. Easiest way to see this is to touch a button in a real navigation bar and slide your finger out of the range and back in. This diff exposes the offset as a prop (I thought touchRetentionOffset was a more informative name than pressRectOffset).

--- Libraries/Components/Touchable/TouchableOpacity.js
+++ Libraries/Components/Touchable/TouchableOpacity.js
@@ -12,6 +12,7 @@
 
 // Note (avik): add @flow when Flow supports spread properties in propTypes
 
+var EdgeInsetsPropType = require('EdgeInsetsPropType');
 var NativeMethodsMixin = require('NativeMethodsMixin');
 var POPAnimationMixin = require('POPAnimationMixin');
 var React = require('React');
@@ -25,6 +26,8 @@ var ensurePositiveDelayProps = require('ensurePositiveDelayProps');
 var flattenStyle = require('flattenStyle');
 var keyOf = require('keyOf');
 var onlyChild = require('onlyChild');
+
+var TOUCH_RETENTION_OFFSET = {top: 20, left: 20, right: 20, bottom: 30};
 
 /**
  * A wrapper for making views respond properly to touches.
@@ -50,7 +53,6 @@ var onlyChild = require('onlyChild');
  * >
  * > If you wish to have to have several child components, wrap them in a View.
  */
-
 var TouchableOpacity = React.createClass({
   mixins: [TimerMixin, Touchable.Mixin, NativeMethodsMixin, POPAnimationMixin],
 
@@ -66,6 +68,7 @@ var TouchableOpacity = React.createClass({
   getDefaultProps: function() {
     return {
       activeOpacity: 0.2,
+      touchRetentionOffset: TOUCH_RETENTION_OFFSET,
     };
   },
 
@@ -138,7 +141,7 @@ var TouchableOpacity = React.createClass({
   },
 
   touchableGetPressRectOffset: function() {
-    return PRESS_RECT_OFFSET;   // Always make sure to predeclare a constant!
+    return this.props.touchRetentionOffset;
   },
 
   touchableGetHighlightDelayMS: function() {
@@ -182,14 +185,6 @@ var TouchableOpacity = React.createClass({
     });
   },
 });
-
-/**
- * When the scroll view is disabled, this defines how far your touch may move
- * off of the button, before deactivating the button. Once deactivated, try
- * moving it back and you'll see that the button is once again reactivated!
- * Move it back and forth several times while the scroll view is disabled.
- */
-var PRESS_RECT_OFFSET = {top: 20, left: 20, right: 20, bottom: 30};
 
 var CHILD_REF = keyOf({childRef: null});
 
