5e794f47c314f6642e3c773895b7c50f3f680676
James Ide <ide@jameside.com>
2015-07-28 21:36:12 -0700

    [Executor] Start to use the Obj-C JSContext instead of the C API
    The Obj-C API is usually easier to work with, and also makes it very easy to use the C API when necessary (performance, for example). This diff just switches the designated initializer of RCTContextExecutor to take a JSContext instead of JSGlobalContextRef, but the old initializer still works if needed.
    
    I was doing some memory leak investigation and it is easier with ARC so I wanted to incrementally move the executor to Obj-C.
    
    Test Plan: Launch UIExplorer, can reload it fine.

--- React/Executors/RCTContextExecutor.m
+++ React/Executors/RCTContextExecutor.m
@@ -39,9 +39,10 @@
 
 @interface RCTJavaScriptContext : NSObject <RCTInvalidating>
 
+@property (nonatomic, strong, readonly) JSContext *context;
 @property (nonatomic, assign, readonly) JSGlobalContextRef ctx;
 
-- (instancetype)initWithJSContext:(JSGlobalContextRef)context;
+- (instancetype)initWithJSContext:(JSContext *)context;
 
 @end
 
@@ -50,25 +51,29 @@
   RCTJavaScriptContext *_self;
 }
 
-- (instancetype)initWithJSContext:(JSGlobalContextRef)context
+- (instancetype)initWithJSContext:(JSContext *)context
 {
   if ((self = [super init])) {
-    _ctx = context;
+    _context = context;
     _self = self;
   }
   return self;
 }
 
+- (JSGlobalContextRef)ctx
+{
+  return _context.JSGlobalContextRef;
+}
+
 - (BOOL)isValid
 {
-  return _ctx != NULL;
+  return _context != nil;
 }
 
 - (void)invalidate
 {
   if (self.isValid) {
-    JSGlobalContextRelease(_ctx);
-    _ctx = NULL;
+    _context = nil;
     _self = nil;
   }
 }
@@ -238,11 +243,11 @@ static NSError *RCTNSErrorFromJSError(JSContextRef context, JSValueRef jsError)
   [javaScriptThread setThreadPriority:[[NSThread mainThread] threadPriority]];
   [javaScriptThread start];
 
-  return [self initWithJavaScriptThread:javaScriptThread globalContextRef:NULL];
+  return [self initWithJavaScriptThread:javaScriptThread context:nil];
 }
 
 - (instancetype)initWithJavaScriptThread:(NSThread *)javaScriptThread
-                        globalContextRef:(JSGlobalContextRef)context
+                                 context:(JSContext *)context
 {
   RCTAssert(javaScriptThread != nil,
             @"Can't initialize RCTContextExecutor without a javaScriptThread");
@@ -257,15 +262,20 @@ static NSError *RCTNSErrorFromJSError(JSContextRef context, JSValueRef jsError)
         return;
       }
       // Assumes that no other JS tasks are scheduled before.
-      JSGlobalContextRef ctx;
       if (context) {
-        ctx = JSGlobalContextRetain(context);
-        strongSelf->_context = [[RCTJavaScriptContext alloc] initWithJSContext:ctx];
+        strongSelf->_context = [[RCTJavaScriptContext alloc] initWithJSContext:context];
       }
     }];
   }
 
   return self;
+}
+
+- (instancetype)initWithJavaScriptThread:(NSThread *)javaScriptThread
+                        globalContextRef:(JSGlobalContextRef)contextRef
+{
+  JSContext *context = contextRef ? [JSContext contextWithJSGlobalContextRef:contextRef] : nil;
+  return [self initWithJavaScriptThread:javaScriptThread context:context];
 }
 
 - (void)setUp
@@ -277,8 +287,8 @@ static NSError *RCTNSErrorFromJSError(JSContextRef context, JSValueRef jsError)
       return;
     }
     if (!strongSelf->_context) {
-      JSGlobalContextRef ctx = JSGlobalContextCreate(NULL);
-      strongSelf->_context = [[RCTJavaScriptContext alloc] initWithJSContext:ctx];
+      JSContext *context = [[JSContext alloc] init];
+      strongSelf->_context = [[RCTJavaScriptContext alloc] initWithJSContext:context];
     }
     [strongSelf _addNativeHook:RCTNativeLoggingHook withName:"nativeLoggingHook"];
     [strongSelf _addNativeHook:RCTNoop withName:"noop"];
